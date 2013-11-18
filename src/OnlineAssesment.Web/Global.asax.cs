ceee2eb14ac3ce41dbfbb100f2e4eb666d8c628a
Lu Rongkai <lurongkai@gmail.com>
2013-11-19 00:21:43 +0800

    update packages. modified login page.

--- src/OnlineAssesment.Web/Global.asax.cs
+++ src/OnlineAssesment.Web/Global.asax.cs
@@ -16,7 +16,7 @@ namespace OnlineAssesment.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            AutofacConfig.RegisterIoc();
+            //AutofacConfig.RegisterIoc();
         }
     }
 }
