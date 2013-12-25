83e86d8dc6056f8666c8bdd2f35497b63f8b7d46
Lu Rongkai <lurongkai@gmail.com>
2013-12-25 18:13:51 +0800

    add sidebar.

---
+++ src/OnlineAssesment.Web/Controllers/TeacherController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineAssesment.Web.Controllers
+{
+    public class TeacherController : Controller
+    {
+        //
+        // GET: /Teacher/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
