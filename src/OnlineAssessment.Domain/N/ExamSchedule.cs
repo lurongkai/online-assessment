7f4d26e956b4cc7279c77ba67599faee86ad70d7
Lu Rongkai <lurongkai@gmail.com>
2014-09-09 18:10:50 +0800

    new domain.

---
+++ src/OnlineAssessment.Domain/N/ExamSchedule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineAssessment.Domain.N
+{
+    public class ExamSchedule : ValueObject
+    {
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/OnlineAssessment.Domain/N/IAggregateMember.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineAssessment.Domain.N
+{
+    public interface IAggregateRoot<T> : IAggregateRoot, IEntity<T>
+      where T : IAggregateRoot
+    {
+    }
+
+    public interface IAggregateRoot : IAggregateMember
+    {
+    }
+
+    public interface IEntity<T> : IAggregateMember<T>, IEntity
+       where T : IAggregateRoot
+    {
+    }
+
+    public interface IEntity : IAggregateMember
+    {
+    }
+
+
+    public interface IAggregateMember<T> : IAggregateMember
+       where T : IAggregateRoot
+    {
+    }
+
+    public interface IAggregateMember
+    {
+    }
+}
